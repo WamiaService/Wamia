bb3b514c01900defc9f05649b06fbfdedd8c1dda
KhouloudNEILY <neilykhouloud@gmail.com>
2023-08-19 14:41:07 +0200

    last version

--- client/components/Provider/AllProviders.jsx
+++ client/components/Provider/AllProviders.jsx
@@ -31,8 +31,8 @@ function AllProviders() {
         const endpoint = category
           
          
-            ? `http://192.168.100.12:3000/provider/search?category=${category}`
-            : `http://192.168.100.12:3000/provider/payprove`;
+            ? `http://192.168.100.2:3000/provider/search?category=${category}`
+            : `http://192.168.100.2::3000/provider/payprove`;
            
            
     
